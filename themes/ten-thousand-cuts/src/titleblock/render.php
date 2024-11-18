efdc61e56734afe2f455f152be62517b55a745a6
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-11-18 12:17:09 -0800

    Update Title Block

--- themes/ten-thousand-cuts/src/titleblock/render.php
+++ themes/ten-thousand-cuts/src/titleblock/render.php
@@ -7,14 +7,16 @@
 <div <?php echo get_block_wrapper_attributes(); ?> data-wp-interactive="tenthousandcuts">
 	<div class="title_block">
 		<div class="title_block__headlines">
-			<h1 class="mb0 mt0"><?php echo get_bloginfo('name'); ?></h1>
+			<a href="<?php echo get_home_url(); ?>">
+				<h1 class="mb0 mt0"><?php echo get_bloginfo('name'); ?></h1>
+			</a>
 			<h2 class="mt0 mb5"><?php echo get_bloginfo('description'); ?></h2>
 		</div>
 		<div class="title_block__menu">
 
 			<div class="title_block__menu--dropdown--container">
 				<div class="title_block__menu--dropdown">
-					<div class="menu_item menu_item_shop">shop</div>
+					<!-- <div class="menu_item menu_item_shop">shop</div> -->
 					<a href="/artwork" class="menu_item menu_item_archives">archives</a>
 					<div class="menu_item menu_item_close" id="CloseMenu"><i class="fa-solid fa-circle-half-stroke"></i></div>
 				</div>
