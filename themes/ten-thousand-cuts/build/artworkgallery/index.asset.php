e29ffac8b0fe1b8c77b9a3dc3e820741ecd71fd3
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-11-19 15:02:04 -0800

    Clean Up Gallery Code

--- themes/ten-thousand-cuts/build/artworkgallery/index.asset.php
+++ themes/ten-thousand-cuts/build/artworkgallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ea4ac05b4cfd64e6115d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cdb364b531a2672c2c8f');
--- themes/ten-thousand-cuts/src/artworkgallery/render.php
+++ themes/ten-thousand-cuts/src/artworkgallery/render.php
@@ -1,12 +1,10 @@
 <section id="artgrid" class="artgrid alignwide">
 
 	<div class="artgrid__strip">
+
 		<?php
-
-
 		$artwork = new WP_Query(array(
 			'post_type' => 'artwork',
-
 			// 'posts_per_page' => -1,
 			'order' => 'ASC',
 			'orderby' => 'rand',
@@ -19,8 +17,6 @@
 					// 'type' => 'numeric'
 				)
 			)
-
-
 		));
 
 		while ($artwork->have_posts()) {
@@ -52,20 +48,14 @@
 
 		?>
 
-
 			<div class="artgrid__art art <?php echo $frameclass ?>" style="background-image: url(<?php echo get_field('hero_image') ?>);">
-
 				<h2 class="artgrid__art__title"><?php echo get_field('title') ?></h2>
 				<a href="<?php echo get_the_permalink() ?>">
-
-
 			</div>
-
 		<?php
 		}
 		// echo paginate_links();
-		// wp_reset_postdata();
-
+		wp_reset_postdata();
 		?>
 
 	</div>
