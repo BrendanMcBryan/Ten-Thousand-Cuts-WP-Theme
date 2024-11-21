847c251bfbe65de7076876c3d80723a52a3d0034
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-11-21 10:24:16 -0800

    Master Live Site Merge

--- themes/ten-thousand-cuts/build/artworkdisplaycontainer/index.asset.php
+++ themes/ten-thousand-cuts/build/artworkdisplaycontainer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ecfbc35557efcca99dd5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c00184b1a9bd57fb44d4');
--- themes/ten-thousand-cuts/build/singleartwork/index.asset.php
+++ themes/ten-thousand-cuts/build/singleartwork/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '757749e3bdabb1b987d1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6b37367c41eef020c118');
