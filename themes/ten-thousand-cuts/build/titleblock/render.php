24a17177dfc3ae6b42ad48a2f067248453c4aa83
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-12-04 13:01:09 -0800

    Clean up title Block and margins

--- themes/ten-thousand-cuts/build/titleblock/render.php
+++ themes/ten-thousand-cuts/build/titleblock/render.php
@@ -7,13 +7,13 @@ $logo = get_template_directory_uri() . '/assets/images/Eris-Icon.svg'
 <div <?php echo get_block_wrapper_attributes(); ?> data-wp-interactive="tenthousandcuts">
 	<div class="title_block">
 		<div class="title_block__logo">
-			<img src="<?php echo $logo ?>" />
+			<a href="<?php echo get_home_url(); ?>"> <img src="<?php echo $logo ?>" id="logo" /> </a>
 		</div>
 		<div class="title_block__headlines">
 			<a href="<?php echo get_home_url(); ?>">
 				<h1 class="mb0 mt0"><?php echo get_bloginfo('name'); ?></h1>
 			</a>
-			<h2 class="mt0 mb5"><?php echo get_bloginfo('description'); ?></h2>
+			<!-- <h2 class="mt0 mb5">< ?php echo get_bloginfo('description'); ?></h2> -->
 		</div>
 		<!-- <div class="title_block__menu">
 
