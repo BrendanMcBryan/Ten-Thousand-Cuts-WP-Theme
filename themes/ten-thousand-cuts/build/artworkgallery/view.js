0e89bfa84684723f7525ee3f19098bd695b25ce2
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-12-05 11:29:02 -0800

    Remove limit on posts that gallery block publishes

---
+++ themes/ten-thousand-cuts/build/artworkgallery/view.js
@@ -0,0 +1,46 @@
+/******/ (() => { // webpackBootstrap
+/*!************************************!*\
+  !*** ./src/artworkgallery/view.js ***!
+  \************************************/
+// import Glide, { Controls } from '@glidejs/glide';
+// import Splide from '@splidejs/splide';
+// // var nextButton = document.querySelector('#next');
+// // var prevButton = document.querySelector('#prev');
+
+// const randomStart = Math.floor(Math.random() * 10 + 1);
+// //random number between 1 and 10.
+
+// var glide = new Glide('#artworkDisplay', {
+//   type: 'slider',
+//   perView: 5,
+//   focusAt: 'center',
+//   startAt: 5,
+// });
+
+// // nextButton.addEventListener('click', function (event) {
+// //   event.preventDefault();
+
+// //   glide.go('>');
+// // });
+
+// // prevButton.addEventListener('click', function (event) {
+// //   event.preventDefault();
+
+// //   glide.go('<');
+// // });
+
+// // glide.mount();
+
+// var splide = new Splide('.splide', {
+//   perPage: 2,
+//   type: 'loop',
+//   height: '68vh',
+
+//   focus: 'center',
+//   autoWidth: true,
+// });
+
+// splide.mount();
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
