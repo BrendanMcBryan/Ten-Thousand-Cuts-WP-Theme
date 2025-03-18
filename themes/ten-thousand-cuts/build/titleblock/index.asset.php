f3a0db61315dd2b1d82633a44cc4b53026631d70
Brendan McBryan <brendan.mcbryan@gmail.com>
2025-03-18 14:59:02 -0700

    Artwork Adjustments

--- themes/ten-thousand-cuts/build/titleblock/index.asset.php
+++ themes/ten-thousand-cuts/build/titleblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b99b89519caabe5124e8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a9c78afe5ff43a1b1c5d');
