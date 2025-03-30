d318c08dcb7f8d2fa4a2687c98458cb15a0a3ca5
Brendan McBryan <brendan.mcbryan@gmail.com>
2025-03-30 11:57:50 -0700

    Update SHop Buttons

--- themes/ten-thousand-cuts/build/artworkgallery/view.js
+++ themes/ten-thousand-cuts/build/artworkgallery/view.js
@@ -1,1 +1,34 @@
-(()=>{const e=document.getElementById("artgrid");let t,o=1,l=!1;t=setInterval((function(){l||(e.scrollTop+=o,(e.scrollTop+e.clientHeight>=e.scrollHeight||e.scrollTop<=0)&&(o*=-1))}),30),e.addEventListener("mouseover",(()=>{l=!0})),e.addEventListener("mouseout",(()=>{l=!1}))})();
+/******/ (() => { // webpackBootstrap
+/*!************************************!*\
+  !*** ./src/artworkgallery/view.js ***!
+  \************************************/
+const scrollableDiv = document.getElementById('artgrid');
+let scrollDirection = 1; // 1 for down, -1 for up
+let scrollInterval;
+let isHovered = false;
+function autoScroll() {
+  if (!isHovered) {
+    scrollableDiv.scrollTop += scrollDirection;
+
+    // Reverse direction if reaching top or bottom
+    if (scrollableDiv.scrollTop + scrollableDiv.clientHeight >= scrollableDiv.scrollHeight || scrollableDiv.scrollTop <= 0) {
+      scrollDirection *= -1;
+    }
+  }
+}
+
+// Start autoscrolling
+scrollInterval = setInterval(autoScroll, 30);
+
+// Pause on hover
+scrollableDiv.addEventListener('mouseover', () => {
+  isHovered = true;
+});
+
+// Resume on mouse leave
+scrollableDiv.addEventListener('mouseout', () => {
+  isHovered = false;
+});
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
