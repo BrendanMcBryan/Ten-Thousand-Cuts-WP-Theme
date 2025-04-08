dbc22958cd49c199e52e03c01203560188546565
Brendan McBryan <brendan.mcbryan@gmail.com>
2025-04-08 14:22:03 -0700

    Test from Desktop

--- themes/ten-thousand-cuts/src/titleblock/render.php
+++ themes/ten-thousand-cuts/src/titleblock/render.php
@@ -25,9 +25,9 @@ $logo = get_template_directory_uri() . '/assets/images/Eris-Icon.svg'
 
 			<a href="/about" class="menu_item menu_item_about">about</a>
 			<a href="/shop" class="menu_item menu_item_shop">shop</a>
-			<a href="/shop" class="menu_item menu_item_shop">new test</a>
+
 		</div>
 
 	</div>
 
-</div> 
+</div>
