37d495b478cd0698cd4de11ac30ab1a74f6e1cbe
Brendan McBryan <brendan.mcbryan@gmail.com>
2025-05-02 16:35:57 -0700

    Adjust Woo Comerce sprting

--- themes/ten-thousand-cuts/build/artworkdisplaycontainer/view.asset.php
+++ themes/ten-thousand-cuts/build/artworkdisplaycontainer/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '9137cb65aeca4429c775');
+<?php return array('dependencies' => array(), 'version' => '5f49ccf7345657d45a24');
--- themes/ten-thousand-cuts/build/singleartwork/index.asset.php
+++ themes/ten-thousand-cuts/build/singleartwork/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5c699c686d58daac8690');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '757749e3bdabb1b987d1');
