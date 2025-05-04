d4ba7c9d7702b23fe261438ca4e0445a0562c786
brendan <brendan.mcbryan@gmail.com>
2025-05-04 11:33:00 -0700

    Update Window Symatry

--- themes/ten-thousand-cuts/build/artworkgallery/index.asset.php
+++ themes/ten-thousand-cuts/build/artworkgallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '55310323210f39fe16a0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '45c54edb25becdb7bba3');
