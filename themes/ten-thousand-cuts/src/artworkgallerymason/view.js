42f73930dcb73594715266e31e030b70ce7a7ea9
Brendan McBryan <brendan.mcbryan@gmail.com>
2025-05-06 06:40:04 -0700

    Prettier files on Desktop

--- themes/ten-thousand-cuts/src/artworkgallerymason/view.js
+++ themes/ten-thousand-cuts/src/artworkgallerymason/view.js
@@ -1,17 +1,13 @@
-import Masonry from "masonry-layout";
-
-
+import Masonry from 'masonry-layout';
 
 var elem = document.querySelector('.artgrid__strip');
-var msnry = new Masonry( elem, {
+var msnry = new Masonry(elem, {
   // options
   itemSelector: '.artgrid__art',
 
-  
   columnWidth: '.grid-sizer',
   gutter: '.gutter-sizer',
-  percentPosition: true
+  percentPosition: true,
 
-// gutter:10,
+  // gutter:10,
 });
-
