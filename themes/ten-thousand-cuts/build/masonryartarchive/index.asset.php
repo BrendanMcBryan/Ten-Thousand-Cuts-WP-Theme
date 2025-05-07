ebd6381dd70cd4beb8418ec6868e005fdb6afbbb
brendan <brendan.mcbryan@gmail.com>
2025-05-07 14:50:12 -0700

    Remove old Masonry Gallery

--- themes/ten-thousand-cuts/build/masonryartarchive/index.asset.php
+++ themes/ten-thousand-cuts/build/masonryartarchive/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd4c9681432dfcb8d3dcf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ad18d7d4b705264ecfba');
